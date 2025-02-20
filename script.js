b2ceac78545bb7365fe579ed843f789178f8ffbe
Kunta-pro <smartedupexa@gmail.com>
2025-02-20 22:40:36 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('donateBtn').addEventListener('click', function() {
+    alert('Thank you for your interest in donating!');
+});
+
+document.getElementById('donateNow').addEventListener('click', function() {
+    alert('Redirecting to donation page...');
+});
